66374372838b30bf54267dbd2661c5b89118cce1
snowdust-eng <123239724+snowdust-eng@users.noreply.github.com>
2023-02-22 22:31:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,18 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Good Morning Life')
+                    streamlit.header('Breakfast Menu')
 streamlit.subheader('Classic English Breakfast')
 streamlit.text('Contains bread,french fries,baked beans,scrambled eggs. Options: Chicken, Ham, Pork')
+streamlit.subheader('Good Morning Life')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.header('Lunch Menu')
+                    streamlit.header('Lunch Menu')
 streamlit.subheader('Continental Bonanza')
 streamlit.text(' Contains Soup, choice of non veg, rice/bread Options: Chicken, fish, beef')
 
-streamlit.header('Dinner Menu')
+                    streamlit.header('Dinner Menu')
 streamlit.subheader('Light and fun')
 streamlit.text(' Options: Soup, Thai noodles, pasta, Chicken rice')
